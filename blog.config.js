4c966f65bb138fdc6028644d9ab7617ee07b4cca
Kyoz <61974760+Kai26@users.noreply.github.com>
2022-05-09 23:03:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
   title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  author: '呱唧',
+  email: 'ozcwss@gmail.com',
+  link: 'https://blog.ygxlj.xyz',
+  description: 'This gonna be an awesome blog.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
