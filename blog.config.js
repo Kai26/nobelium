34ae8cb919f3264e78e0186cbcf52546cb1cbd08
Kyoz <61974760+Kai26@users.noreply.github.com>
2022-05-10 13:32:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: '', // The link to generate OG image, don't end with a slash
   socialLink: 'https://t.me/ygxgj',
   seo: {
     keywords: ['Blog', 'Apple','iTunes Gift Card', 'Notion'],
